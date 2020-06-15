8f6e801deca3b5469e0e37ad0c554986a1a97f8b
Cole <cole.trumbo@smoothstack.com>
2020-06-15 14:54:10 -0400

    just the component from material-ui

---
+++ client/src/components/CollapsibleTable/CollapsibleTable.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const CollapsibleTable = () => (
+  <div>
+    CollapsibleTable Component
+  </div>
+);
+
+CollapsibleTable.propTypes = {};
+
+CollapsibleTable.defaultProps = {};
+
+export default CollapsibleTable;
