873e853ed14ca5162cd2fa8ef7d329b5b4a1b734
WilliamMarchek <39922188+wilbobraggins@users.noreply.github.com>
2020-06-14 21:59:48 -0400

    added rudux for switching views

--- client/src/components/Schedule/Schedule.jsx
+++ client/src/components/Schedule/Schedule.jsx
@@ -3,6 +3,9 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import Paper from '@material-ui/core/Paper';
 import { ViewState } from '@devexpress/dx-react-scheduler';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel'
 import {
     Scheduler,
     WeekView,
@@ -11,6 +14,22 @@ import {
 } from '@devexpress/dx-react-scheduler-material-ui';
 import * as scheduleActions from "../../services/Schedule/actions"
 
+/* const ViewSwitcher = ({
+    currentViewName,
+    onChange,
+}) => (
+        <RadioGroup
+            aria-label="Views"
+            style={{ flexDirection: 'row' }}
+            name="views"
+            value={currentViewName}
+            onChange={onChange}
+        >
+            <FormControlLabel value="Month" control={<Radio />} label="Month" />
+            <FormControlLabel value="Week" control={<Radio />} label="Week" />
+            <FormControlLabel value="Day" control={<Radio />} label="Day" />
+        </RadioGroup>
+    ); */
 function mapStateToProps(state) {
     return {
         currentView: state.scheduleReducer.currentView
@@ -22,18 +41,46 @@ function mapDispatchToProps(dispatch) {
         actions: bindActionCreators(scheduleActions, dispatch),
     };
 }
+const ViewSwitcher = ({ onChange, currentViewName }) => (
+    <RadioGroup
+        aria-label="Views"
+        style={{ flexDirection: 'row' }}
+        name="views"
+        value={currentViewName}
+        onChange={onChange}
+    >
+        <FormControlLabel value="Month" control={<Radio />} label="Month" />
+        <FormControlLabel value="Week" control={<Radio />} label="Week" />
+        <FormControlLabel value="Day" control={<Radio />} label="Day" />
+    </RadioGroup>
+);
 
+const CurrentView = (props) => {
+    const [viewName, setViewName] = React.useState("Month");
 
-export default function Schedule() {
+    const viewChange = (e, index) => {
+        const { actions } = props;
+        setViewName(index);
+        actions.switchView(index);
+    };
+
     return (
         <Paper>
+
+            <ViewSwitcher 
+            currentViewName={viewName}
+            onChange={viewChange}/>
             <Scheduler>
-                <ViewState />
+                <ViewState currentViewName={viewName} />
                 <MonthView />
+                <WeekView
+                    startDayHour={10}
+                    endDayHour={19}
+                />
                 <Appointments />
             </Scheduler>
         </Paper>
-    );
-}
+    )
+};
 
-/* export default connect(mapStateToProps, mapDispatchToProps)(Schedule); */
+export default connect(mapStateToProps, mapDispatchToProps)(CurrentView);
--- client/src/services/Schedule/reducer.js
+++ client/src/services/Schedule/reducer.js
@@ -3,10 +3,8 @@ import { SWITCH_VIEW } from './actionTypes'
 export default function scheduleReducer(state = {}, action) {
     switch (action.type) {
         case SWITCH_VIEW:
-            return { ...state, currentView: action.data };
+            return { ...state, currentView: action.data};
         default:
             return state;
-
     }
-
 }
