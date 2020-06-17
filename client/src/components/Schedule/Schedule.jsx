870edcc05af321e80c538770535a372d7c6f13bd
WilliamMarchek <39922188+wilbobraggins@users.noreply.github.com>
2020-06-16 21:33:29 -0400

    fixed calendar and schedule were in wrong components

--- client/src/components/Schedule/Schedule.jsx
+++ client/src/components/Schedule/Schedule.jsx
@@ -1,74 +1,63 @@
-import React, { useState } from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import Paper from '@material-ui/core/Paper';
-import { ViewState } from '@devexpress/dx-react-scheduler';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel'
+import React from "react";
+import DateFnsUtils from '@date-io/date-fns';
 import {
-    Scheduler,
-    WeekView,
-    MonthView,
-    Appointments,
-    DayView,
-} from '@devexpress/dx-react-scheduler-material-ui';
-import * as scheduleActions from "../../services/Schedule/actions"
+    MuiPickersUtilsProvider,
+    KeyboardTimePicker,
+    KeyboardDatePicker,
+} from '@material-ui/pickers';
+import { Container, Grid } from '@material-ui/core';
 
 
-function mapStateToProps(state) {
-    return {
-        currentView: state.scheduleReducer.currentView
-    };
-}
+export default function DatePicker() {
+    // The first commit of Material-UI
+    const [selectedDate, setSelectedDate] = React.useState(new Date());
 
-function mapDispatchToProps(dispatch) {
-    return {
-        actions: bindActionCreators(scheduleActions, dispatch),
-    };
-}
-const ViewSwitcher = ({ onChange, currentViewName }) => (
-    <RadioGroup
-        aria-label="Views"
-        style={{ flexDirection: 'row',
-        justifyContent: 'center' }}
-        name="views"
-        value={currentViewName}
-        onChange={onChange}
-    >
-        <FormControlLabel value="Month" control={<Radio />} label="Month" />
-        <FormControlLabel value="Week" control={<Radio />} label="Week" />
-        <FormControlLabel value="Day" control={<Radio />} label="Day" />
-    </RadioGroup>
-);
-
-const CurrentView = (props) => {
-    const [viewName, setViewName] = React.useState("Month");
-
-    const viewChange = (e, index) => {
-        const { actions } = props;
-        setViewName(index);
-        actions.switchView(index);
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
     };
 
     return (
-        <Paper>
 
-            <ViewSwitcher 
-            currentViewName={viewName}
-            onChange={viewChange}/>
-            <Scheduler>
-                <ViewState currentViewName={viewName} />
-                <MonthView />
-                <WeekView
-                    startDayHour={10}
-                    endDayHour={19}
-                />
-                <DayView />
-                <Appointments />
-            </Scheduler>
-        </Paper>
-    )
-};
+        <Container>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <Grid container justify="space-around">
+                    <KeyboardDatePicker
+                        disableToolbar
+                        variant="inline"
+                        format="MM/dd/yyyy"
+                        margin="normal"
+                        id="date-picker-inline"
+                        label="Date picker inline"
+                        value={selectedDate}
+                        onChange={handleDateChange}
+                        KeyboardButtonProps={{
+                            'aria-label': 'change date',
+                        }}
+                    />
+                    <KeyboardDatePicker
+                        margin="normal"
+                        id="date-picker-dialog"
+                        label="Date picker dialog"
+                        format="MM/dd/yyyy"
+                        value={selectedDate}
+                        onChange={handleDateChange}
+                        KeyboardButtonProps={{
+                            'aria-label': 'change date',
+                        }}
+                    />
+                    <KeyboardTimePicker
+                        margin="normal"
+                        id="time-picker"
+                        label="Time picker"
+                        value={selectedDate}
+                        onChange={handleDateChange}
+                        KeyboardButtonProps={{
+                            'aria-label': 'change time',
+                        }}
+                    />
+                </Grid>
+            </MuiPickersUtilsProvider>
+        </Container>
 
-export default connect(mapStateToProps, mapDispatchToProps)(CurrentView);
+    );
+}
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,8 +1,8 @@
 import AppointmentHeader from './AppointmentHeader/header';
 import AvatarBtn from './Avatar/AvatarBtn';
-import DatePicker from './Calendar/Calendar';
+import Calendar from './Calendar/Calendar';
 import MessagesTabs from './Messages/MessagesTabs';
-import Schedule from './Schedule/Schedule'
+import DatePicker from './Schedule/Schedule'
 import SideBar from './SideBar/SideBar';
 import LogoutButton from './LogoutButton/LogoutButton';
 import CollapsibleTable from './CollapsibleTable/CollapsibleTable';
@@ -12,7 +12,7 @@ export {
     AvatarBtn,
     DatePicker,
     MessagesTabs,
-    Schedule,
+    Calendar,
     SideBar,
     CollapsibleTable,
     LogoutButton
