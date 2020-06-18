af5e33b180b00143dc81f0f7055fd00d8d19a01c
Hoff <haffedmcnair@gmail.com>
2020-06-18 17:23:31 -0400

    added logo to login

--- client/src/utils/loginApi.js
+++ client/src/utils/loginApi.js
@@ -9,7 +9,6 @@ export const loginApi = {
    * @returns Promise
    */
   isValidUser: (fullname, id) => {
-    console.log("cat");
     fullname = fullname.replace('.', ' ');
     const queryString = apiRoute + `contacts?$select=firstname,ss_patientid,lastname&$filter=contains(fullname, '${fullname}') and contains(ss_patientid, '${id}')`;
     return adalApiFetch(axios, queryString, getConfig)
