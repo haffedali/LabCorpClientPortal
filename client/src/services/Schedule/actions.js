dea3615ce2f80620ddd4cada066c8b0f51037b38
WilliamMarchek <39922188+wilbobraggins@users.noreply.github.com>
2020-06-19 16:06:42 -0400

    dynamics request for appointments

--- client/src/services/Schedule/actions.js
+++ client/src/services/Schedule/actions.js
@@ -1,16 +1,29 @@
-import {SWITCH_VIEW} from './actionTypes'
+import {SWITCH_VIEW, GET_DATA} from './actionTypes'
+import { appointmentsApi } from "../../utils";
 
-export const switchView = (view) => {
+export const switchView = (view, id) => {
     return dispatch => {
+        appointmentsApi
+        .query(id.id)
+        .then((res) => {
+            console.log(res)
+            const contactId = res.data
+            dispatch(_getAppointments(contactId))
+        })
         dispatch(_switchView(view))
     }
 }
-
-
 
 const _switchView = (view) => {
     return {
         type: SWITCH_VIEW,
         data: view
+    }
+}
+
+const _getAppointments =  (id) => {
+    return {
+        type: GET_DATA,
+        data: id
     }
 }
