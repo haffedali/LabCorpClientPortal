8a45e5f187d3e08dff138f29063ad916d1826033
Cole <cole.trumbo@smoothstack.com>
2020-06-20 14:33:27 -0400

    WIP ugly. get labresult data and display

--- client/src/pages/Results/Results.js
+++ client/src/pages/Results/Results.js
@@ -2,63 +2,48 @@ import React, { Component } from 'react';
 
 import { CollapsibleTable } from '../../components'
 import { Typography,
-  TextField
+  TextField,
+  Button
 } from '@material-ui/core';
 
-import { makeStyles } from '@material-ui/core/styles';
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     '& .MuiTextField-root': {
-//       margin: theme.spacing(1),
-//       width: '25ch',
-//     },
-//   },
-//   table: {
-//     minWidth: 650,
-//   },
-// }));
-
-// const classes = useStyles();
-
-function createData(name, date) {
-  return {
-    name,
-    date,
-    items: [
-      { item: 'Red Blood Cell Count', numericValue: '100', unit: 'per droplet' },
-      { item: 'Iron', numericValue: '0.5', unit: 'g' },
-      { item: 'Chemical X', numericValue: '1', unit: 'tsp' },
-    ],
-  };
-}
+import { testResultsApi } from '../../utils/testResultsApi';
 
 function isFound(str, q) {
   return str.toLowerCase().indexOf(q) > -1
 }
 
 const rows = [
-  createData('Hemoglobin', "2019-11-01"),
-  createData('Hemoglobin', "2018-11-01"),
-  createData('Hemoglobin', "2017-11-01"),
-];
-
+  {
+    name: "hemoglobin", 
+    date: "2019-12-12", 
+    items: [
+      {
+        item: "Red Blood Cells", 
+        numericValue: "1", 
+        unit: "g",
+      },
+    ]
+  },
+]
 
 class Results extends React.Component {
   state = {
     search: "",
+    apiData: {poop: "is pooop"},
   }
   render() {
-      const matchingRows = rows.filter((row) => isFound(row.name, this.state.search) || isFound(row.date, this.state.search))
-      return (
+    const matchingRows = rows.filter((row) => isFound(row.name, this.state.search) || isFound(row.date, this.state.search))
+    return (
       <div>
         <Typography component="h1" variant="h3">
           Test Results
         </Typography>
-        {/*<form className={classes.root} noValidate autoComplete="off">*/}
         <form noValidate autoComplete="off" >
           <TextField id="search" label="Search tests" type="search" onChange={(e) => this.setState({search: e.target.value.toLowerCase()})} />
         </form>
+        <p>API data: </p>
+        <p>{JSON.stringify(this.state.apiData)}</p>
+        <Button onClick={() => {testResultsApi.doSomething().then((data) => { this.setState({apiData: data}) })} }>Refresh</Button>
         <CollapsibleTable className="minWidth: 650" rows={matchingRows} ></CollapsibleTable>
       </div>
     )
--- client/src/utils/testResultsApi.js
+++ client/src/utils/testResultsApi.js
@@ -1,7 +1,88 @@
 import axios from "axios";
-import  {adalApiFetch} from '../adalConfig';
-import {getConfig, apiRoute } from './APIHeaders';
+import { AuthenticationContext, adalFetch, withAdalLogin } from 'react-adal';
+
+export const adalConfig = {
+  tenant: '58bf51ec-ca22-48d9-ad93-e9d44309b27a',
+  clientId: 'efa93954-ca45-47d5-9f92-b628f05b4871',
+  endpoints: {
+    api: 'https://sswilbobraggins.crm.dynamics.com/',
+  },
+  cacheLocation: 'localStorage',
+  redirectUri: 'http://localhost:3000'
+};
+ 
+export const authContext = new AuthenticationContext(adalConfig);
+ 
+/**
+ * 
+ * @param axios fetch 
+ * @param string url 
+ * @param getConfig options 
+ */
+export const adalApiFetch = (fetch, url, options) =>
+  adalFetch(authContext, adalConfig.endpoints.api, fetch, url, options);
+
+
+export const withAdalLoginApi = withAdalLogin(authContext, adalConfig.endpoints.api);
+
+export const apiRoute = 'https://sswilbobraggins.api.crm.dynamics.com/api/data/v9.1/';
+
+export const getConfig = {
+    method: 'get',
+    'OData-MaxVersion': 4.0,
+    'OData-Version': 4.0,
+    Accept: 'application/json',
+    'Content-Type': 'application/json; charset=utf-8',
+    headers: {
+        'Prefer': "odata.include-annotations=*"
+    }
+};
+    // `https://sswilbobraggins.api.crm.dynamics.com/api/data/v9.1/contacts?$select=firstname&$filter=contains(fullname, 'Richard Daley')&$expand=Contact_Phonecalls($select=subject)`
+    // after '&$expand=' is where you put the name of the relationship you are looking for
+    //&$expand=Lab_Results
+
 
 export const testResultsApi = {
-    
+  doSomething : () => {
+    return adalApiFetch(axios, apiRoute + "ss_labresults", getConfig)
+  }
 }
+
+function fetchData() {
+  let req = new XMLHttpRequest();
+  req.open("GET", "https://sswilbobraggins.api.crm.dynamics.com" + "/api/data/v9.1/ss_labresults", true);
+  req.setRequestHeader("OData-MaxVersion", "4.0");
+  req.setRequestHeader("OData-Version", "4.0");
+  req.setRequestHeader("Accept", "application/json");
+  req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+  req.setRequestHeader("Prefer", "odata.include-annotations=\"*\"");
+  req.onreadystatechange = function() {
+      if (this.readyState === 4) {
+          req.onreadystatechange = null;
+          if (this.status === 200) {
+              let results = JSON.parse(this.response);
+              return results.value
+              // for (let i = 0; i < results.value.length; i++) {
+              //   let {
+              //     ss_date, 
+              //     ss_date@OData.Community.Display.V1.FormattedValue, 
+              //     _ss_appointment_value@OData.Community.Display.V1.FormattedValue, 
+              //    } = results.value[i]
+              //    if(rows)
+                // {ss_labresultid: results.value[i]["ss_labresultid"]};
+                // ss_labresultid,
+                // ss_numericvalue,
+                // ss_name, 
+                // ss_unit, 
+                // ss_date, 
+                // ss_date@OData.Community.Display.V1.FormattedValue, 
+                // _ss_appointment_value@OData.Community.Display.V1.FormattedValue, 
+              // }
+          } else {
+              console.log(this.statusText);
+          }
+      }
+  };
+  req.send();
+}
+
