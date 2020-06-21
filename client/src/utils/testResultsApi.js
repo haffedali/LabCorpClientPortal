2a98b9962d4eada58b49e74077812a6c8cb133c2
Cole <cole.trumbo@smoothstack.com>
2020-06-21 18:08:25 -0400

    WIP worst state ever, about to start refactoring

--- client/src/utils/testResultsApi.js
+++ client/src/utils/testResultsApi.js
@@ -2,19 +2,15 @@ import axios from "axios";
 import {getConfig, apiRoute } from './APIHeaders';
 import  {adalApiFetch} from '../adalConfig';
 
-// `https://sswilbobraggins.api.crm.dynamics.com/api/data/v9.1/contacts?$select=firstname&$filter=contains(fullname, 'Richard Daley')&$expand=Contact_Phonecalls($select=subject)`
-    // after '&$expand=' is where you put the name of the relationship you are looking for
-    //&$expand=Lab_Results
-
-
 export const testResultsApi = {
   doSomething : () => {
     return adalApiFetch(axios, apiRoute + "ss_labresults", getConfig)
   },
   getGroupedResults : () => {
     const results = adalApiFetch(axios, apiRoute + "ss_labresults", getConfig)
+    const keys = [{}]
     results.forEach(value => {
-      return
+      
     });
   }
 }
