9d19eedbb1fc578cd6525b775a055dbff7331905
Hoff <haffedmcnair@gmail.com>
2020-06-21 11:22:20 -0400

    ui changes for messages

--- client/src/components/Messages/Messages.styles.js
+++ client/src/components/Messages/Messages.styles.js
@@ -2,7 +2,6 @@ import {makeStyles} from '@material-ui/styles';
 
 export const useStyles = makeStyles((theme) => ({
     container: {
-
         width: "80vw",
         height: '85vh',
         padding: '0px'  
