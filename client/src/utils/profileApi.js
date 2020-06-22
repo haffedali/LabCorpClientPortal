3ef658cce13205353489713cb3c4dc5c49c28c13
Hoff <haffedmcnair@gmail.com>
2020-06-22 16:31:19 -0400

    cleaned out profileApi

--- client/src/utils/profileApi.js
+++ client/src/utils/profileApi.js
@@ -3,18 +3,4 @@ import { adalApiFetch } from "../adalConfig";
 import { getConfig, apiRoute } from "./APIHeaders";
 
 export const profileApi = {
-  /**
-   * @param object userObject
-   * @returns Promise
-   */
-  update: (userObj) => {
-    fullname = fullname.replace(".", " ");
-    const queryString =
-      apiRoute +
-      `contacts?$select=firstname,ss_patientid,
-      address1_line1,address1_city,address1_stateorprovince,
-      address1_postalcode,address1_country,mobilephone,
-      emailaddress1,lastname&$filter=contains(fullname, '${fullname}') and contains(ss_patientid, '${id}')&$expand=ss_contact_ss_insuranceplan_Patient($select=ss_name)`;
-    return adalApiFetch(axios, queryString, getConfig);
-  },
 };
