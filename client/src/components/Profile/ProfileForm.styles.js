8a42c8637551f8badade1efa7a549f6edb3f285a
TRamanh Le <tramanh.le@smoothstack.com>
2020-07-02 14:26:27 -0400

    Fixing the visuals for the Home page

--- client/src/components/Profile/ProfileForm.styles.js
+++ client/src/components/Profile/ProfileForm.styles.js
@@ -6,7 +6,7 @@ export const useStyles = makeStyles((theme) => ({
         margin: theme.spacing(2),
         width: '50ch',
       },
-      '.MuiFilledInput-underline': {
+      '& .MuiFilledInput-underline': {
         color: theme.PRIMARY,
       },
     },
