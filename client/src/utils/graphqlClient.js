245ca36453374b4533881bc118651bdc30618feb
danielbarone <dev.danielbarone@gmail.com>
2020-07-16 00:56:13 -0400

    testdeploy

--- client/src/utils/graphqlClient.js
+++ client/src/utils/graphqlClient.js
@@ -5,12 +5,13 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 import { services_uri } from './services_uri';
 
 export const cache = new InMemoryCache();
+//    uri: process.env.REACT_APP_SERVICES_URI || services_uri,
 
 const client = new ApolloClient({
   cache,
   link: new HttpLink({
     credentials: "include",
-    uri: process.env.REACT_APP_SERVICES_URI || services_uri,
+    uri: services_uri,
   })
 });
 
