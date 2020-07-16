39a30dc735a5ddfab52e31348863fa9ed1698122
danielbarone <dev.danielbarone@gmail.com>
2020-07-16 02:04:19 -0400

    merge and push to devops

--- client/src/utils/services_uri.js
+++ client/src/utils/services_uri.js
@@ -1,2 +1,2 @@
-export const services_uri='https://600c30a9f713.ngrok.io/graphql';
+export const services_uri='https://4d2034f5a3f1.ngrok.io/graphql';
 export const adalRedirectUri='https://azlccpafswa.azurewebsites.net/';
